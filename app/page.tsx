d5c130728557e2f9c56e26e96c401bb2ddd09d8c
Fathir <fathirbimashabri@gmail.com>
2024-10-24 22:04:41 +0700

    remove header

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,4 @@
 import ChartView from "./components/ChartView";
-import Header from "./components/Header";
 import Image from "next/image";
 import background from "../public/images/bg.png";
 import Foto from "./components/Foto";
